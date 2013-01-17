ed54ad444f4140f24b9b1f0d1d25c229a860ad66
Wagner J Goncalves <wagnerjgoncalves@gmail.com>
2013-01-17 08:56:29 -0200

    Change App.js and package.json

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
-
-/**
- * Module dependencies.
- */
 var express = require('express'),
     routes = require('./routes'),
     api = require('./routes/api');
 
-
 var app = module.exports = express();
 
-// Configuration
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
@@ -29,13 +23,10 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
-// Routes
 app.get('/', routes.index);
 
-// Partials routes
 app.get('/partials/:name', routes.partials);
 
-// Post routes
 var post = new api.Post();
 app.get('/posts/list', post.list);
 app.get('/posts/show/:id', post.show);
@@ -43,11 +34,9 @@ app.post('/posts/create', post.create);
 app.put('/posts/update/:id', post.update);
 app.delete('/posts/destroy/:id', post.destroy);
 
-// redirect all others to the index (HTML5 history)
 app.get('*', routes.index);
 
-// Start server
-var port = process.env.PORT || 5000; // Use the port that Heroku provides or default to 5000
+var port = process.env.PORT || 5000; 
 app.listen(port, function() {
   console.log("Express server listening on port %d in %s mode", port, app.settings.env);
 });
